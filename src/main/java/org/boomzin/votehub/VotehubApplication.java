78f5c7b1f58a0d547d73c7adf4f11447468bcd05
boomzin windows work <boomzin@gmail.com>
2021-09-02 14:14:16 +0300

    Initial commit

---
+++ src/main/java/org/boomzin/votehub/VotehubApplication.java
@@ -0,0 +1,13 @@
+package org.boomzin.votehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotehubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VotehubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/boomzin/votehub/VotehubApplicationTests.java
@@ -0,0 +1,13 @@
+package org.boomzin.votehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotehubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
